cf5d4f5fa295591ac225be9d333a382e92279b87
nsedrickm <nsedrick101@gmail.com>
2020-03-01 17:40:31 +0100

    pricing section

--- js/main.js
+++ js/main.js
@@ -46,38 +46,47 @@ $(document).ready(function () {
   $(".team-1").hover(function () {
     $(".team-1-cbd").toggleClass("bg-gradient");
     $(".team-1-default").toggleClass("d-none");
-    $(".team-1-show").toggleClass("d-none")
+    $(".team-1-show").toggleClass("d-none");
   });
 
   $(".team-2").hover(function () {
     $(".team-2-cbd").toggleClass("bg-gradient");
     $(".team-2-default").toggleClass("d-none");
-    $(".team-2-show").toggleClass("d-none")
+    $(".team-2-show").toggleClass("d-none");
   });
 
   $(".team-3").hover(function () {
     $(".team-3-cbd").toggleClass("bg-gradient");
     $(".team-3-default").toggleClass("d-none");
-    $(".team-3-show").toggleClass("d-none")
+    $(".team-3-show").toggleClass("d-none");
   });
 
   $(".team-4").hover(function () {
     $(".team-4-cbd").toggleClass("bg-gradient");
     $(".team-4-default").toggleClass("d-none");
-    $(".team-4-show").toggleClass("d-none")
+    $(".team-4-show").toggleClass("d-none");
   });
 
   /* toggle plan slider and cards */
 
   $(".btn-1").click(function () {
     $(this).toggleClass("btn-primary active");
-    $(".btn-2").toggleClass("btn-primary active")
+    $(".btn-2").toggleClass("btn-primary active");
+    $(".price-1").html("$10");
+    $(".price-2").html("$20");
+    $(".price-3").html("$50");
   });
 
   $(".btn-2").click(function () {
     $(this).toggleClass("btn-primary active");
-    $(".btn-1").toggleClass("btn-primary active")
+    $(".btn-1").toggleClass("btn-primary active");
+    $(".price-1").html("$99");
+    $(".price-2").html("$199");
+    $(".price-3").html("$299");
+
   });
+
+
 });
 
 //footer date placement
