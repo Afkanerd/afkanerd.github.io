3c10e69687a50d5d9a07518476f73b27e6e20014
nSedrickm <nsedrick101@gmail.com>
2020-08-30 10:16:08 +0100

    major deletes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ particlesJS.load('particles-js', '/assets/particlesjs-config.json', function () 
 
 
 
-
 $(document).ready(function () {
 
   /* timeline section js */
@@ -17,32 +16,12 @@ $(document).ready(function () {
   });
   /* timeline section js */
 
-  /* phone display caroussel animation */
-  $('.app-dev').on('mouseenter', function (e) {
-    $(this).find("i").toggleClass('animated flip slower');
-  })
-
-  /* phone display carousel animation */
-
   /* logo animation */
 
   $(".logo-sm").on('mouseover', function (e) {
     $(this).toggleClass('animated flip slower');
   });
   /* logo animation end */
-
-  $(".intro-section").on('mouseleave', function (e) {
-    $(this).find(".col-md-6:first").toggleClass('');
-  });
-
-  /*scroll down button */
-  $("#scroll-down").click(function (e) {
-    window.scroll({
-      top: 500,
-      behavior: 'smooth'
-    });
-  })
-  /* logo animation */
 
   /* number count effect */
   $('.count').each(function () {
@@ -58,135 +37,6 @@ $(document).ready(function () {
   });
   /* number count effect */
 
-  /* toggle view for each team member */
-  $(".team-1").hover(function () {
-    $(this).toggleClass("animated tada");
-    $(".team-1-default").toggleClass("d-none");
-    $(".team-1-show").toggleClass("d-none");
-  });
-
-  $(".team-2").hover(function () {
-    $(this).toggleClass("animated tada");
-    $(".team-2-default").toggleClass("d-none");
-    $(".team-2-show").toggleClass("d-none");
-  });
-
-  $(".team-3").hover(function () {
-    $(this).toggleClass("animated tada");
-    $(".team-3-default").toggleClass("d-none");
-    $(".team-3-show").toggleClass("d-none");
-  });
-
-  $(".team-4").hover(function () {
-    $(this).toggleClass("animated tada");
-    $(".team-4-default").toggleClass("d-none");
-    $(".team-4-show").toggleClass("d-none");
-  });
-
-  /* toggle plan slider and cards */
-
-  $(".btn-1").click(function () {
-    $(this).toggleClass("btn-primary");
-    $(".btn-2").toggleClass("btn-primary");
-    $(".price-1").html("$10");
-    $(".price-2").html("$20");
-    $(".price-3").html("$50");
-  });
-
-  $(".btn-2").click(function () {
-    $(this).toggleClass("btn-primary active");
-    $(".btn-1").toggleClass("btn-primary active");
-    $(".price-1").html("$99");
-    $(".price-2").html("$199");
-    $(".price-3").html("$299");
-
-  });
-
-  /* animation for autolplay slider caroussel using slick */
-  $('.autoplay').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    arrows: false,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-
-  /* floating carousel section animation with slick */
-  /* floating profile photo  carousel */
-  $('.reviewer-img').slick({
-    dots: false,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    arrows: false,
-    easing: 'ease',
-    fade: true
-
-  });
-
-  $('.reviewer-info').slick({
-    dots: false,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    fade: true,
-    cssEase: 'ease',
-    adaptiveHeight: true,
-    nextArrow: '<span class="slick-next"><i class="fas fa-chevron-right bg-gradient text-light p-2 rounded-circle"></i></span>',
-    prevArrow: '<span class="slick-prev"><i class="fas fa-chevron-left bg-gradient text-light p-2 rounded-circle"></i></span>'
-  });
-
-  $('.review-text').slick({
-    dots: false,
-    infinite: true,
-    fade: true,
-    speed: 1000,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    arrows: false,
-    easing: 'ease'
-  });
-
-  /* sync both carousels */
-  $(".reviewer-info > .slick-next").on('click', function (e) {
-    $(".reviewer-img").slick("slickNext");
-    $(".review-text").slick("slickNext");
-
-  });
-
-  $('.reviewer-info > .slick-prev').on('click', function (e) {
-    $(".reviewer-img").slick('slickPrev');
-    $(".review-text").slick("slickPrev");
-  });
 
 });
 
