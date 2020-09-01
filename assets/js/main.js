6a944e35de7c26ee269f45a80d6453237b86cb87
nSedrickm <nsedrick101@gmail.com>
2020-09-01 19:20:48 +0100

    add mousescroll support for swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,9 @@ var technologies_swiper = new Swiper('.swiper-container', {
   autoplay: {
     delay: 5000,
   },
+  mousewheel: {
+    invert: true,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
